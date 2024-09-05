cefbc794fecb106db68f3240b0b37cb770210b14
Lethal friend <12736763+lethal_friend@user.noreply.gitee.com>
2024-09-05 09:19:07 +0800

    病床mapper层编写

---
+++ ward_management_system/src/main/java/com/st/mapper/BedMapper.java
@@ -0,0 +1,20 @@
+package com.st.mapper;
+
+import com.st.bean.Bed;
+
+import java.util.List;
+
+public interface BedMapper {
+    //添加
+    int insert (Bed b);
+    //根据ID删除
+    int delete(Long bid);
+    //修改
+    int update(Bed b);
+    //查询所有护士
+    List<Bed> selectAll();
+    //根据病房号查询
+    Bed selectByBnumber(String bnumber);
+    Bed selectByWnumber(String wnumber);
+    Bed selectByBid(Long bid);
+}
