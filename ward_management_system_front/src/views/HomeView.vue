9948bd6b76b961adf4e6a8637f6c9d3adf10e47b
Lethal friend <12736763+lethal_friend@user.noreply.gitee.com>
2024-09-06 00:44:40 +0800

    完善UI界面

--- ward_management_system_front/src/views/HomeView.vue
+++ ward_management_system_front/src/views/HomeView.vue
@@ -1,30 +1,32 @@
 <template>
   <div class="common-layout">
     <el-container>
-      <el-header>Header</el-header>
+      <el-header  style="padding: 0; margin: 0;">
+        <el-header style="background-color: #1e1c23; color: white; padding: 0; margin: 0;width: 100%;">Header</el-header>
+      </el-header>
       <el-container>
         <el-aside>
-          <el-menu active-text-color="#ffd04b" background-color="#545c64" class="el-menu-vertical-demo"
+          <el-menu active-text-color="#ffd04b" background-color="#1e1c23" class="el-menu-vertical-demo"
             default-active="2" text-color="#fff" @select="toPage">
-            <el-menu-item index="/index">
-              <el-icon>
-                <House />
-              </el-icon>
-              首页
-            </el-menu-item>
+            <el-menu-item index="/index"><el-icon><House /></el-icon>首页</el-menu-item>
             <el-sub-menu index="2">
               <template #title>
-                <el-icon>
-                  <el-icon>
-                    <HomeFilled />
-                  </el-icon>
-                </el-icon>
-                <span>病房管理</span>
+                <el-icon><el-icon><User /></el-icon></el-icon><span>患者管理</span>
               </template>
-              <el-menu-item index="/patient">患者管理</el-menu-item>
-              <el-menu-item index="/dept">科室管理</el-menu-item>
-              <el-menu-item index="/nurse">护士管理</el-menu-item>
-              <el-menu-item index="/ward">病房管理</el-menu-item>
+              <el-menu-item index="/patient" style="background-color: #3a3646;">患者管理</el-menu-item>
+            </el-sub-menu>
+            <el-sub-menu index="3">
+              <template #title>
+                <el-icon><el-icon><Avatar /></el-icon></el-icon><span>员工管理</span>
+              </template>
+              <el-menu-item index="/dept" style="background-color: #3a3646;">科室管理</el-menu-item>
+              <el-menu-item index="/nurse" style="background-color: #3a3646;">护士管理</el-menu-item>
+            </el-sub-menu>
+            <el-sub-menu index="4">
+              <template #title>
+                <el-icon><el-icon><HomeFilled /></el-icon></el-icon><span>病房管理</span>
+              </template>
+              <el-menu-item index="/ward" style="background-color: #3a3646;">病房管理</el-menu-item>
             </el-sub-menu>
           </el-menu>
         </el-aside>
