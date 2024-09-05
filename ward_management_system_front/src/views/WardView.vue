9f679a4bbde772efdbe9972608e2f19f37a3b890
Lethal friend <12736763+lethal_friend@user.noreply.gitee.com>
2024-09-05 21:03:17 +0800

    病房添加

--- ward_management_system_front/src/views/WardView.vue
+++ ward_management_system_front/src/views/WardView.vue
@@ -1,6 +1,6 @@
 <template>
     <el-card style="max-width: 600px">
-        <el-button type="success" style="margin-bottom: 10px;" @click="showAddDialog">添加</el-button>
+        <el-button type="success" style="margin-bottom: 10px;" @click="addDialogShow = true;">添加</el-button>
         <el-table :data="wardList" border style="width: 100%">
             <el-table-column prop="wid" label="ID" width="50px" />
             <el-table-column prop="wnumber" label="病房号" />
@@ -18,6 +18,27 @@
             </el-table-column>
         </el-table>
     </el-card>
+
+    <!-- 添加对话框开始 -->
+    <el-dialog v-model="addDialogShow" title="添加病房" width="500">
+        <el-form>
+            <el-form-item label="病房号" lable-width="20%">
+                <el-input v-model="wardAdd.wnumber" autocomplete="off" />
+            </el-form-item>
+            <el-form-item label="所属科室" lable-width="20%">
+                <el-input v-model="wardAdd.did" autocomplete="off" />
+            </el-form-item>
+        </el-form>
+        <template #footer>
+            <div class="dialog-footer">
+                <el-button @click="addDialogShow = false">取消</el-button>
+                <el-button type="primary" @click="insert">确认</el-button>
+            </div>
+        </template>
+    </el-dialog>
+    <!-- 添加对话框结束 -->
+
+
 </template>
 <script setup>
 import wardApi from '@/api/wardApi';
@@ -27,12 +48,49 @@ import { ElMessage } from 'element-plus';
 
 const wardList = ref([]);
 
+const wardAdd = ref({
+    wnumber: '',
+    did: ''
+});
+
+const addDialogShow = ref(false);
+
 function selectAll() {
     wardApi.selectAll()
         .then(resp => {
             wardList.value = resp.data; 
         });
     
+}
+
+//定义方法完成病房添加
+function insert() {
+    wardApi.insert(wardAdd.value)
+        .then(resp => {
+            if (resp.code == 10000) {
+                ElMessage({
+                    message: resp.msg,
+                    type: 'success',
+                    duration: 1200
+                });
+                //隐藏对话框
+                addDialogShow.value = false;
+                //清空对话框中的数据
+                wardAdd.value = {
+                    dname: '',
+                    dlocation: ''
+                }
+                //刷新表格数据
+                selectAll();
+            } else {
+                ElMessage({
+                    message: resp.msg,
+                    type: 'error',
+                    duration: 2000
+                });
+            }
+
+        });
 }
 
 selectAll();
