69be09f057b19697c2de024a8149cf3fb562f749
Lethal friend <12736763+lethal_friend@user.noreply.gitee.com>
2024-09-06 09:27:56 +0800

    后端病床Controler层编写并测试

---
+++ ward_management_system/src/main/java/com/st/controller/BedController.java
@@ -0,0 +1,59 @@
+package com.st.controller;
+
+
+import com.st.bean.Bed;
+import com.st.bean.RespBean;
+import com.st.exception.SteduException;
+import com.st.service.BedService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/user/bed")
+public class BedController {
+    @Autowired
+    public BedService bedService;
+
+    @PostMapping
+    public RespBean insert(@RequestBody Bed bed) throws SteduException {
+        //添加
+        if (bedService.insert(bed)) {
+            return RespBean.ok("添加成功");
+        } else {
+            return RespBean.error("添加失败，请重新添加");
+        }
+    }
+
+    @DeleteMapping("/{bid}")
+    public RespBean delete(@PathVariable("bid") Long bid) throws SteduException {
+        //添加
+        if (bedService.delete(bid)) {
+            return RespBean.ok("删除成功");
+        } else {
+            return RespBean.error("删除失败，请重新添加");
+        }
+    }
+    @PutMapping
+    public RespBean update(@RequestBody Bed bed) throws SteduException {
+        //添加
+        if (bedService.update(bed)) {
+            return RespBean.ok("修改成功");
+        } else {
+            return RespBean.error("修改失败，请重新添加");
+        }
+    }
+
+    @GetMapping("/{bid}")
+    public RespBean selectByDid(@PathVariable("bid") Long bid) {
+        Bed b = bedService.selectByBid(bid);
+        return RespBean.ok("",b);
+    }
+    @GetMapping
+    public RespBean selelctAll() {
+        List<Bed> bedList = bedService.selectAll();
+        return RespBean.ok("",bedList);
+    }
+}
