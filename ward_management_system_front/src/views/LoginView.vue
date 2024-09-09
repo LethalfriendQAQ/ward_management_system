75fcd4617e7eae80e18dc44b77a05eaeffc8f82b
Lethal friend <12736763+lethal_friend@user.noreply.gitee.com>
2024-09-09 19:37:25 +0800

    前端登录校验不能为空

--- ward_management_system_front/src/views/LoginView.vue
+++ ward_management_system_front/src/views/LoginView.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="container">
         <h1>用户登录</h1>
-        <el-form rel="form" label-width="60px">
-            <el-form-item label="用户名">
+        <el-form :model="admin" :rules="rules" ref="form" label-width="60px">
+            <el-form-item label="用户名" prop="username" label-width="65px">
                 <el-input type="text" v-model="admin.username"></el-input>
             </el-form-item>
-            <el-form-item label="密码">
+            <el-form-item label="密码" prop="password" label-width="65px">
                 <el-input type="password" v-model="admin.password"></el-input>
             </el-form-item>
             <el-form-item>
@@ -21,33 +21,50 @@ import router from '@/router';
 import { ElMessage } from 'element-plus';
 import { ref } from 'vue';
 
+// 表单数据
 const admin = ref({
     username: '',
     password: ''
 });
 
-//登录
+// 表单验证规则
+const rules = {
+    username: [
+        { required: true, message: '用户名不能为空', trigger: 'blur' }
+    ],
+    password: [
+        { required: true, message: '密码不能为空', trigger: 'blur' }
+    ]
+};
+
+// 表单引用
+const form = ref(null);
+
+// 登录逻辑
 function login() {
-    adminApi.login(admin.value)
-    .then(resp => {
-        if(resp.code == 10000) {
-            //弹出提示信息
-            ElMessage({
-                    message: resp.msg,
-                    type: 'success',
-                    duration: 1200,
-                    onClose: () => {
-                        //保存jwt到sessionStorage
-                        sessionStorage.setItem('token', resp.data);
-                        //跳转到后台首页
-                        router.push('/index');
-                    }
-                });
-            
+    form.value.validate((valid) => {
+        if (valid) {
+            // 如果表单验证通过，调用登录 API
+            adminApi.login(admin.value).then(resp => {
+                if (resp.code === 10000) {
+                    ElMessage({
+                        message: resp.msg,
+                        type: 'success',
+                        duration: 1200,
+                        onClose: () => {
+                            // 保存 JWT 到 sessionStorage
+                            sessionStorage.setItem('token', resp.data);
+                            // 跳转到后台首页
+                            router.push('/index');
+                        }
+                    });
+                } else {
+                    ElMessage.error(resp.msg);
+                }
+            });
         } else {
-            ElMessage.error(resp.msg);
+            ElMessage.error('不能为空');
         }
-        
     });
 }
 
@@ -62,5 +79,8 @@ function login() {
     padding-top: 0px;
     margin: auto;
     margin-top: 150px;
-    background-color: lightskyblue;
-  }</style>
+    /* background-color: rgba(0, 0, 0, 0.1); */
+}
+
+
+</style>
